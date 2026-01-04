4409791e595d630234787956cf114c510ea8a982
Pushparaj Singh Parmar <psinghparmar42@gmail.com>
2026-01-04 13:44:35 +0530

    coverimage issue handling

--- project/src/controllers/user.controller.js
+++ project/src/controllers/user.controller.js
@@ -31,14 +31,21 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(409, "Email or UserName Already exists");
   }
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  console.log(avatarLocalPath);
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
-  console.log(coverImageLocalPath);
+  //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is required");
   }
   const avatar = await uploadOnCloudinary(avatarLocalPath);
   const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
   if (!avatar) {
     throw new ApiError(400, "Avatar is required");
   }
